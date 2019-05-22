8a79101ce739726820d2a27a43ad83aa4a8fbd9e
JoseEdeNeto <jose.ede.neto@gmail.com>
2019-05-22 11:18:19 -0300

    hello world running

--- helloworld/helloworld.js
+++ helloworld/helloworld.js
@@ -2,8 +2,9 @@ import http from 'http';
 
 http.createServer((req, res) => {
     res.writeHead(200);
-    res.write('Hello world');
-    res.end();
+    res.write('Hello world is running');
+    setTimeout(() => {
+        res.write('Hello world is done');
+        res.end();
+    }, 5000);
 }).listen(8080);
-
-console.log('listening on port 8080');
